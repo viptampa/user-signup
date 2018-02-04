e0172a6ffabca05516d2f9db45bcd75167b35424
viptampa <viptampa@gmail.com>
2018-02-03 19:20:14 -0500

    working revision 1

--- main.py
+++ main.py
@@ -69,16 +69,20 @@ def validateinput():
     vpassword_error = ''
     email_error = ''
     welcomemsg = "Welcome, " + username + "!"
+    badvalues = (" ", "@@", "..")
+    periodtoomuch = 0
+    atsigntoomuch = 0
+    spacetoomuch = 0
 
-    if len(username) < minlength or len(username) > 20:
+    if len(username) < minlength or len(username) > maxlength:
         username_error = "That's not a valid username"
         username = ""
 
-    if len(password) < minlength or len(password) > 20:
+    if len(password) < minlength or len(password) > maxlength:
         password_error = "That's not a valid password"
         password = ""
 
-    if len(vpassword) < minlength or len(vpassword) > 20:
+    if len(vpassword) < minlength or len(vpassword) > maxlength:
         vpassword_error = "Passwords don't match"
         vpassword = ""
     
@@ -86,7 +90,22 @@ def validateinput():
         vpassword_error = "Passwords don't match"
         vpassword = ""
 
-    if not username_error and not password_error and not vpassword_error:
+    if len(email) > 0:
+        if len(email) < minlength or len(email) > maxlength:
+            email_error = "That's not a valid email"
+            email = ""
+        for letter in email:
+            if letter == ".":
+                periodtoomuch += 1 
+            if letter == "@":
+                atsigntoomuch += 1
+            if letter == " ":
+                spacetoomuch += 1
+        if periodtoomuch > 1 or atsigntoomuch > 1 or spacetoomuch > 0:
+            email_error = "That's not a valid email"
+            email = ""
+
+    if not username_error and not password_error and not vpassword_error and not email_error:
         return welcomemsg
     else:
         return form.format(username_error=username_error, password_error
